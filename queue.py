22660b94662928931a66ad1bc6ed5f046e77929f
eternal1025 <eternal1025@163.com>
2018-03-09 08:15:16 +0800

    queue

---
+++ queue.py
@@ -0,0 +1,27 @@
+
+'''
+	Queue:FIFO
+'''
+
+class Queue(object):
+	def __init__(self):
+		self._elements = []
+
+	def __repr__(self):
+		return '<Queue size={!r}>.'.format(len(self))
+
+	def enqueue(self, x):
+		self._elements.append(x)
+
+	def dequeue(self):
+		return self._elements.pop(0)
+
+	def is_empty(self):
+		return len(self) == 0
+
+	def __len__(self):
+		return len(self._elements)
+
+	def clear(self):
+		self._elements.clear()
+
